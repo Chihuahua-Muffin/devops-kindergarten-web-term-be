3a64b06f62bad371ec70866057cd0e3641c08013
JerryK <kskyung0624@gmail.com>
2021-06-17 14:25:56 +0900

    first commit

---
+++ src/main/java/com/devopskindergarten/xtermssh/XtermSshApplication.java
@@ -0,0 +1,13 @@
+package com.devopskindergarten.xtermssh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtermSshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XtermSshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devopskindergarten/xtermssh/XtermSshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devopskindergarten.xtermssh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XtermSshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
